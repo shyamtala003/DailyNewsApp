899444df7b834d94ba2b1294f389df87e27492b1
shyamtala003 <shyamtala003@gmail.com>
2023-01-14 00:54:59 +0530

    news api is intigrated

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,41 +2,39 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, url } = this.props;
     return (
-      <div class="max-w-[300px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-[300px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img
-            class="rounded-t-lg"
-            src="https://tinypng.com/images/social/website.jpg"
-            alt=""
-          />
+          <img className="rounded-t-lg" src={imageUrl} alt="" />
         </a>
-        <div class="p-5">
+        <div className="p-5">
           <a href="#">
-            <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+            <h5 className="mb-2 text-lg leading-5 font-bold tracking-tight text-gray-900 dark:text-white">
+              {String(title).length > 45 ? title.slice(0, 45) + "..." : title}
             </h5>
           </a>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {String(description).length > 85
+              ? description.slice(0, 85) + "..."
+              : description}
           </p>
           <a
-            href="#"
-            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            href={url}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Read more
             <svg
               aria-hidden="true"
-              class="w-4 h-4 ml-2 -mr-1"
+              className="w-4 h-4 ml-2 -mr-1"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </a>
