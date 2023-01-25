ad0cd684be54d62416952f0e20ccca62f17343eb
shyamtala003 <shyamtala003@gmail.com>
2023-01-25 22:13:10 +0530

    loader componet fixs

--- src/components/NewsContainer.jsx
+++ src/components/NewsContainer.jsx
@@ -7,8 +7,10 @@ let NewsContainer = ({ country, category, pageSize, SetProgress }) => {
   const [article, setArticle] = useState([]);
   const [page, setPage] = useState(1);
   const [totalResult, settotalResult] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   let updateNews = async () => {
+    setLoading(true);
     document.title =
       String(category).slice(0, 1).toUpperCase() + String(category).slice(1);
 
@@ -20,6 +22,7 @@ let NewsContainer = ({ country, category, pageSize, SetProgress }) => {
 
     setArticle(dataJson.articles);
     settotalResult(dataJson.totalResults);
+    setLoading(false);
     SetProgress(100);
   };
 
@@ -28,13 +31,16 @@ let NewsContainer = ({ country, category, pageSize, SetProgress }) => {
   }, []);
 
   let fetchMoreData = async () => {
+    setLoading(true);
+    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=db76d553d0384c75a5fcc72b0956ffe1&page=${
+      page + 1
+    }&pagesize=${pageSize}`;
     setPage(page + 1);
-
-    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=db76d553d0384c75a5fcc72b0956ffe1&page=${page}&pagesize=${pageSize}`;
     let data = await fetch(url);
     let dataJson = await data.json();
     setArticle(article.concat(dataJson.articles));
     settotalResult(dataJson.totalResults);
+    setLoading(false);
   };
 
   return (
@@ -46,8 +52,8 @@ let NewsContainer = ({ country, category, pageSize, SetProgress }) => {
       <InfiniteScroll
         dataLength={article.length}
         next={fetchMoreData}
-        hasMore={article.length <= totalResult ? true : false}
-        loader={<Loading />}
+        hasMore={article.length <= totalResult}
+        loader={"."}
       >
         <div className="mx-6 my-8  grid justify-items-center max-w-[100vw] overflow-x-hidden  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 gap-8">
           {article.map((el) => {
@@ -73,6 +79,7 @@ let NewsContainer = ({ country, category, pageSize, SetProgress }) => {
             );
           })}
         </div>
+        {loading && <Loading />}
       </InfiniteScroll>
     </>
   );
